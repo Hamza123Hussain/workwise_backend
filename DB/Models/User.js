1387c33b17a411fe22916bf73d2444faf526c8fa
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-22 20:16:21 +0500

    storing all data for the user

---
+++ DB/Models/User.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+
+const UserSchema = new mongoose.Schema({
+  Name: {
+    type: String,
+    required: true,
+  },
+  Email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  _id: {
+    type: String,
+  },
+  imageUrl: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+// Create and export the User model
+const User = mongoose.model('User', UserSchema)
+export default User
