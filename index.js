f98638b4e06126bca0d0fa5a52458601f3a0f042
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-23 02:44:56 +0500

    new path added

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ import express from 'express'
 import { DB_CONNECTED } from './DB_Connect.js'
 import cors from 'cors'
 import AuthRouter from './DB/Router/AuthRouter.js'
+import TimeRouter from './DB/Router/TimeRouter.js'
 const app = express()
 app.use(express.json())
 app.use(cors())
 app.use('/Api/Auth', AuthRouter)
+app.use('/Api/Time', TimeRouter)
 DB_CONNECTED()
+app.listen(8000, () => {
+  console.log('port is on')
+})
