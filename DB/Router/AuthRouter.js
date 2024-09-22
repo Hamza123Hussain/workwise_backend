aa46899df8d2ba0046ecdc4a70057213b5b5fde4
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-22 20:16:08 +0500

    auth paths connected

--- DB/Router/AuthRouter.js
+++ DB/Router/AuthRouter.js
@@ -1,10 +1,14 @@
 import { Router } from 'express'
 import { upload } from '../../MulterConfig.js'
+import { RegisterUser } from '../Controller/Auth/SignUp.js'
+import { Login } from '../Controller/Auth/SignIn.js'
+import { Signout } from '../Controller/Auth/SignOut.js'
+import { ResetPass } from '../Controller/Auth/ResetPass.js'
 
 const AuthRouter = Router()
 
-AuthRouter.post('/Signup', upload.single('Image'))
-AuthRouter.get('/SignIn')
-AuthRouter.get('/Signout')
-AuthRouter.post('/Reset')
+AuthRouter.post('/Signup', upload.single('Image'), RegisterUser)
+AuthRouter.get('/SignIn', Login)
+AuthRouter.get('/Signout', Signout)
+AuthRouter.post('/Reset', ResetPass)
 export default AuthRouter
