44535350050229aef93befa1176d59f0b2837fc3
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-22 20:15:21 +0500

    storing config files

---
+++ Config.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv'
+dotenv.config()
+const apiKey = process.env.FIREBASE_API_KEY
+const authDomain = process.env.FIREBASE_AUTH_DOMAIN
+const projectId = process.env.FIREBASE_PROJECT_ID
+const storageBucket = process.env.FIREBASE_STORAGE_BUCKET
+const messagingSenderId = process.env.FIREBASE_MESSAGING_SENDER_ID
+const appId = process.env.FIREBASE_APP_ID
+const Mongo_url = process.env.Mongo_url
+export {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  Mongo_url,
+}
