3bfee887d71d78a64e0c7f3260c8bba71e47824a
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-22 20:15:56 +0500

    mongo connected

--- DB_Connect.js
+++ DB_Connect.js
@@ -1,10 +1,9 @@
 import mongoose from 'mongoose'
+import { Mongo_url } from './Config.js'
 
 export const DB_CONNECTED = async () => {
   try {
-    const data = await mongoose.connect(
-      `mongodb+srv://hamzahussain14hh:Hamza1208@cluster0.ubbaz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-    )
+    const data = await mongoose.connect(Mongo_url)
     if (data) console.log('DB CONNECTED')
   } catch (error) {
     console.log('DB ERROR', error)
