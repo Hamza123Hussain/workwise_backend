99e522bce0d43906aac0c9fb2f4cdd4673c0610f
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-23 14:50:15 +0500

    update task of a user

---
+++ DB/Controller/Task/UpdateTask.js
@@ -0,0 +1,32 @@
+import { TaskModel } from '../../Models/Task.js'
+import { User } from '../../Models/User.js'
+export const TaskUpdated = async (req, res) => {
+  const { id, Email, progressupdate } = req.body
+  try {
+    const UserExist = await User.findOne({ Email })
+    if (UserExist) {
+      const TaskExist = await TaskModel.findById(id)
+      if (!TaskExist) {
+        return res.status(404).json({ message: 'Task not found' })
+      }
+      // Update the task
+      const TaskUpdated = await TaskModel.findByIdAndUpdate(
+        id,
+        { progress: progressupdate },
+        { new: true } // This returns the updated document
+      )
+      // Return the updated task
+      return res
+        .status(200)
+        .json({ message: 'Task updated successfully', task: TaskUpdated })
+    } else {
+      return res.status(404).json({ message: 'User does not exist' })
+    }
+  } catch (error) {
+    console.error('Error updating task:', error)
+    // Return a generic error message to the client
+    return res
+      .status(500)
+      .json({ message: 'An error occurred while updating the task' })
+  }
+}
