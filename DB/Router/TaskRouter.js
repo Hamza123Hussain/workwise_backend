8b43bf9cc13d96f114c5f9f30e9738eb77b29ad0
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-23 14:49:12 +0500

    all needed paths added

---
+++ DB/Router/TaskRouter.js
@@ -0,0 +1,16 @@
+import { Router } from 'express'
+import { CreateTask } from '../Controller/Task/CreateTask.js'
+import { TaskUpdated } from '../Controller/Task/UpdateTask.js'
+import { AllTasks } from '../Controller/Task/AllTasks.js'
+import { UserTasks } from '../Controller/Task/UserTasks.js'
+import { DeleteTask } from '../Controller/Task/DeleteTask.js'
+
+const TaskRouter = Router()
+
+TaskRouter.post('/CreateNewTask', CreateTask)
+TaskRouter.post('/UpdateTask', TaskUpdated)
+TaskRouter.get('/AllTasks', AllTasks)
+TaskRouter.get('/GetUserTasks', UserTasks)
+TaskRouter.delete('/DeleteTask', DeleteTask)
+
+export default TaskRouter
