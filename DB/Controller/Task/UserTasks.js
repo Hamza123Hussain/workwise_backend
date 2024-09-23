53b47790eec08334a207af52e63346d3c3f2b520
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-23 14:50:03 +0500

    getting tasks of a single user

---
+++ DB/Controller/Task/UserTasks.js
@@ -0,0 +1,27 @@
+import { TaskModel } from '../../Models/Task.js'
+import { User } from '../../Models/User.js'
+
+export const UserTasks = async (req, res) => {
+  const { Email } = req.body
+  try {
+    // Check if the user exists
+    const UserExist = await User.findOne({ Email })
+    if (UserExist) {
+      // Retrieve all tasks for the specific user
+      const AllTasks = await TaskModel.find({ Email })
+      if (AllTasks.length === 0) {
+        return res.status(404).json({ message: 'No tasks found for this user' })
+      }
+      return res
+        .status(200)
+        .json({ message: 'Tasks retrieved successfully', AllTasks })
+    } else {
+      return res.status(404).json({ message: 'User does not exist' })
+    }
+  } catch (error) {
+    console.error('Error retrieving tasks:', error)
+    return res
+      .status(500)
+      .json({ message: 'An error occurred while retrieving the tasks' })
+  }
+}
