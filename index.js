8ac3734f8433eef876161d46b6f1594d60cccfe2
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-23 14:48:17 +0500

    added new router for tasks

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import express from 'express'
 import { DB_CONNECTED } from './DB_Connect.js'
 import cors from 'cors'
 import AuthRouter from './DB/Router/AuthRouter.js'
-import TimeRouter from './DB/Router/TimeRouter.js'
+import TaskRouter from './DB/Router/TaskRouter.js'
+import AttendanceRouter from './DB/Router/AttendanceRouter.js'
 const app = express()
 app.use(express.json())
 app.use(cors())
 app.use('/Api/Auth', AuthRouter)
-app.use('/Api/Time', TimeRouter)
+app.use('/Api/Attendance', AttendanceRouter)
+app.use('/Api/Task', TaskRouter)
 DB_CONNECTED()
 app.listen(8000, () => {
   console.log('port is on')
