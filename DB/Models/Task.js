9eecaf7c10c5cda8cb97313a6c9eb9bf5cca19d6
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-23 17:16:42 +0500

    id attribute added

--- DB/Models/Task.js
+++ DB/Models/Task.js
@@ -1,6 +1,9 @@
 import mongoose from 'mongoose'
 
 const TaskSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+  },
   name: {
     // Changed to camelCase for consistency
     type: String,
