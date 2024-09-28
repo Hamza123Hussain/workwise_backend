4c46ed1c4aaf5cea498b2504a771338505c015a0
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-28 17:44:25 +0500

    Update FireBaseConfig.js

--- FireBaseConfig.js
+++ FireBaseConfig.js
@@ -1,14 +1,24 @@
 import { initializeApp } from 'firebase/app'
 import { getStorage } from 'firebase/storage'
 import { getAuth } from 'firebase/auth'
+import {
+  apiKey,
+  appId,
+  authDomain,
+  messagingSenderId,
+  projectId,
+  storageBucket,
+} from './Config.js'
 const firebaseConfig = {
-  apiKey: 'AIzaSyC-ud1vUtc-c_Fah6wWeJhK06hEma-BjbI',
-  authDomain: 'workwise-86c48.firebaseapp.com',
-  projectId: 'workwise-86c48',
-  storageBucket: 'workwise-86c48.appspot.com',
-  messagingSenderId: '139878436868',
-  appId: '1:139878436868:web:8451c8e53390fb77f22551',
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
 }
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 export const Storage = getStorage(app)
