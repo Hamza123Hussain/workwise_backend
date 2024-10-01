0fc1103ac99b2e479c649200df27e1d46116e4f8
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-01 14:47:12 +0500

    getting current attendance only

---
+++ DB/Controller/Attendance/CurrentAttendance.js
@@ -0,0 +1,32 @@
+import { AttendanceModel } from '../../Models/Attendance.js'
+import { User } from '../../Models/User.js'
+
+export const UserAttendance = async (req, res) => {
+  const { Email } = req.query
+  try {
+    // Check if the user exists by querying the User model with the email
+    const ExistUser = await User.findOne({ Email })
+
+    if (ExistUser) {
+      // Fetch attendance records for the given user's email where checkInStatus is true
+      const allAttendance = await AttendanceModel.find({
+        Email: ExistUser.Email,
+        checkInStatus: true, // Add filter for checkInStatus
+      })
+
+      // If no records are found, return a 404 error
+      if (!allAttendance || allAttendance.length === 0) {
+        return res.status(404).json({ message: 'No attendance records found' })
+      }
+
+      // Respond with success message and the fetched attendance data
+      return res.status(200).json(allAttendance)
+    } else {
+      // If user doesn't exist, respond with 404
+      return res.status(404).json({ message: 'User not found' })
+    }
+  } catch (error) {
+    // Catch any errors and return a 500 status code
+    return res.status(500).json({ message: error.message })
+  }
+}
