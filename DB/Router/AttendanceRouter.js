904fcded7259e2e0c4923f1585d89e753982dfa0
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-01 14:49:33 +0500

    new path

--- DB/Router/AttendanceRouter.js
+++ DB/Router/AttendanceRouter.js
@@ -4,6 +4,7 @@ import { UpdateAttendance } from '../Controller/Attendance/UpdateAttendance.js'
 
 import { AllUser } from '../Controller/Attendance/AllUserAttendance.js'
 import { UserAttendance } from '../Controller/Attendance/UserAttendance.js'
+import { CurrentAttendance } from '../Controller/Attendance/CurrentAttendance.js'
 
 const AttendanceRouter = Router()
 
@@ -11,5 +12,5 @@ AttendanceRouter.post('/NewAttendace', NewAttendance)
 AttendanceRouter.post('/UpdateAttendance', UpdateAttendance)
 AttendanceRouter.get('/UserAttendance', UserAttendance)
 AttendanceRouter.get('/AllAttendance', AllUser)
-
+AttendanceRouter.get('/CurrentAttendance', CurrentAttendance)
 export default AttendanceRouter
