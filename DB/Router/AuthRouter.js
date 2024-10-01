ea0b694ecfb843a12b0979a6ba606745b52cb542
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-01 17:20:56 +0500

    new path added

--- DB/Router/AuthRouter.js
+++ DB/Router/AuthRouter.js
@@ -6,6 +6,7 @@ import { Signout } from '../Controller/Auth/SignOut.js'
 import { ResetPass } from '../Controller/Auth/ResetPass.js'
 import { GetUser } from '../Controller/Auth/GetUser.js'
 import { UpdateUser } from '../Controller/Auth/UpdateUser.js'
+import { AllUsers } from '../Controller/Auth/AllUsers.js'
 
 const AuthRouter = Router()
 
@@ -14,5 +15,6 @@ AuthRouter.post('/SignIn', Login)
 AuthRouter.get('/Signout', Signout)
 AuthRouter.post('/Reset', ResetPass)
 AuthRouter.get('/GetUser', GetUser)
+AuthRouter.get('/AllUsers', AllUsers)
 AuthRouter.post('/UpdateUser', upload.single('Image'), UpdateUser)
 export default AuthRouter
