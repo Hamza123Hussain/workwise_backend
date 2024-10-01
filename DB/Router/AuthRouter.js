bb5559a7ba9be55c768fea87cffbb13aa95f9712
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-01 12:57:26 +0500

    Update AuthRouter.js

--- DB/Router/AuthRouter.js
+++ DB/Router/AuthRouter.js
@@ -5,6 +5,7 @@ import { Login } from '../Controller/Auth/SignIn.js'
 import { Signout } from '../Controller/Auth/SignOut.js'
 import { ResetPass } from '../Controller/Auth/ResetPass.js'
 import { GetUser } from '../Controller/Auth/GetUser.js'
+import { UpdateUser } from '../Controller/Auth/UpdateUser.js'
 
 const AuthRouter = Router()
 
@@ -13,5 +14,5 @@ AuthRouter.post('/SignIn', Login)
 AuthRouter.get('/Signout', Signout)
 AuthRouter.post('/Reset', ResetPass)
 AuthRouter.get('/GetUser', GetUser)
-AuthRouter.post('/UpdateUser', upload.single('Image'), RegisterUser)
+AuthRouter.post('/UpdateUser', upload.single('Image'), UpdateUser)
 export default AuthRouter
