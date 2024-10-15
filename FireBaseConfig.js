4b6d53c04805ec5e0df97b6030d3ad84395d9ba6
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-15 16:45:31 +0500

    Update FireBaseConfig.js

--- FireBaseConfig.js
+++ FireBaseConfig.js
@@ -1,21 +1,16 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
-import { getStorage } from 'firebase/storage'
-import { getAuth } from 'firebase/auth'
-import {
-  apiKey,
-  appId,
-  authDomain,
-  messagingSenderId,
-  projectId,
-  storageBucket,
-} from './Config.js'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: apiKey,
-  authDomain: authDomain,
-  projectId: projectId,
-  storageBucket: storageBucket,
-  messagingSenderId: messagingSenderId,
-  appId: appId,
+  apiKey: 'AIzaSyBMhx9w7jL4RFsRi8BFV35uerE7cGacxJU',
+  authDomain: 'hrmsystem-93957.firebaseapp.com',
+  projectId: 'hrmsystem-93957',
+  storageBucket: 'hrmsystem-93957.appspot.com',
+  messagingSenderId: '390369426414',
+  appId: '1:390369426414:web:dfde11460e743c7cb36cff',
 }
 
 // Initialize Firebase
