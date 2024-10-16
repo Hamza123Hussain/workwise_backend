190881e5c85dc71a4e0b8e446b65b55e601a6457
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-16 16:54:04 +0500

    Update TaskRouter.js

--- DB/Router/TaskRouter.js
+++ DB/Router/TaskRouter.js
@@ -5,6 +5,7 @@ import { AllTasks } from '../Controller/Task/AllTasks.js'
 import { UserTasks } from '../Controller/Task/UserTasks.js'
 import { DeleteTask } from '../Controller/Task/DeleteTask.js'
 import { GetSingleTask } from '../Controller/Task/GetSingleTask.js'
+import { MarkDone } from '../Controller/Task/MarkDone.js'
 
 const TaskRouter = Router()
 
@@ -14,5 +15,5 @@ TaskRouter.get('/AllTasks', AllTasks)
 TaskRouter.get('/GetUserTasks', UserTasks)
 TaskRouter.delete('/DeleteTask', DeleteTask)
 TaskRouter.get('/singleTask', GetSingleTask)
-
+TaskRouter.put('/MarkDone', MarkDone)
 export default TaskRouter
