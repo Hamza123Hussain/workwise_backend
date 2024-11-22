a36e780c84cf935957dadc8cbbf061bfe54098ca
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-11-22 14:48:26 +0500

    fixed the code and now otp verification is working

--- DB/Controller/Otp/verifyOtp.js
+++ DB/Controller/Otp/verifyOtp.js
@@ -1,40 +1,32 @@
+import bcrypt from 'bcryptjs' // Import bcrypt for OTP comparison
 import Otp from '../../Models/Otp.js'
 import { User } from '../../Models/User.js'
-
 export const VerifyOtp = async (req, res) => {
   const { email, otp } = req.body
-
-  // if (!email || !otp) {
-  //   return res
-  //     .status(400)
-  //     .json({ success: false, message: 'Email and OTP are required.' })
-  // }
-
+  if (!email || !otp) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'Email and OTP are required.' })
+  }
   try {
-    // Find the OTP record in the database
-    const otpRecord = await Otp.findOne({ otp })
-    const UserExists = await User.findOne({ email })
-    if (UserExists) {
-      if (!otpRecord) {
-        return res
-          .status(404)
-          .json({ success: false, message: 'OTP not found or expired.' })
-      }
-
-      // Check if OTP matches
-      if (otpRecord.otp !== otp) {
-        return res.status(400).json({ success: false, message: 'Invalid OTP.' })
-      }
-
-      // If OTP is correct, return success
+    // Find the OTP record in the database for the provided email
+    const otpRecord = await Otp.findOne({ email })
+    if (!otpRecord) {
+      return res
+        .status(404)
+        .json({ success: false, message: 'OTP not found or expired.' })
+    }
+    // Use bcrypt to compare the provided OTP with the hashed OTP in the database
+    const isOtpValid = await bcrypt.compare(otp, otpRecord.otp)
+    if (isOtpValid) {
+      // OTP is valid
       return res
         .status(200)
         .json({ success: true, message: 'OTP verified successfully.' })
     } else {
-      return res.status(404).json('No User Found')
+      // OTP is invalid
+      return res.status(400).json({ success: false, message: 'Invalid OTP.' })
     }
-
-    // Check if OTP record exists
   } catch (error) {
     console.error('Error verifying OTP:', error)
     return res
