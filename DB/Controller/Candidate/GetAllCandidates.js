87b06472d86adeb6f32805ec1e83cbb13c18554b
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-02 18:31:07 +0500

    this function would get all the candidates

---
+++ DB/Controller/Candidate/GetAllCandidates.js
@@ -0,0 +1,23 @@
+import CandidateModel from '../../Models/Candidates.js'
+import { User } from '../../Models/User.js'
+export const GetCandidates = async (req, res) => {
+  const { Email } = req.body
+  try {
+    const UserExist = await User.find({ Email })
+    if (UserExist) {
+      const CandidatesFound = await CandidateModel.find()
+      if (CandidatesFound) {
+        res.status(200).json(CandidatesFound)
+      } else {
+        res.status(404).json('No Candidates Has Been Found.')
+      }
+    }
+    res.status(404).json('User Not Authenticated To Acess This')
+  } catch (error) {
+    // Log and send an error response
+    console.error('Error creating candidate:', error)
+    return res
+      .status(500)
+      .json({ message: 'Error creating candidate', error: error.message })
+  }
+}
