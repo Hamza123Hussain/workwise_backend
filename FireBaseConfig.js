3f2983be268dcafafff8a3b76c4c40cad26bcc77
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-03 18:53:39 +0500

    added Realtime database

--- FireBaseConfig.js
+++ FireBaseConfig.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app'
 import { getStorage } from 'firebase/storage'
 import { getAuth } from 'firebase/auth'
+import { getDatabase } from 'firebase/database' // Import Realtime Database
 import {
   apiKey,
   appId,
@@ -9,6 +10,7 @@ import {
   projectId,
   storageBucket,
 } from './Config.js'
+
 const firebaseConfig = {
   apiKey: apiKey,
   authDomain: authDomain,
@@ -20,5 +22,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
+
+// Export Firebase services
 export const auth = getAuth(app)
 export const Storage = getStorage(app)
+export const database = getDatabase(app) // Export Realtime Database
