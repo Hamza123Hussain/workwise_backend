ca857c4f1d443aac10edc953c28bda89c54c9086
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-05 15:15:58 +0500

    Update Notice.js

--- DB/Models/Notice.js
+++ DB/Models/Notice.js
@@ -4,7 +4,7 @@ const NoticeSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   date: { type: Date, default: Date.now },
-  author: { type: String, required: false }, // Optional field for who created the notice
+  author: { type: String, required: false, default: 'ADMIN' }, // Optional field for who created the notice
 })
 
 const Notice = mongoose.model('Notice', NoticeSchema)
