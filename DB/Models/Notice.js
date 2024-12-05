57184f3428467e430e4923b15134c931e4a8866b
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-05 14:38:00 +0500

    schema for the notice has been made

---
+++ DB/Models/Notice.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+
+const NoticeSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  date: { type: Date, default: Date.now },
+  author: { type: String, required: false }, // Optional field for who created the notice
+})
+
+const Notice = mongoose.model('Notice', NoticeSchema)
+
+export default Notice
