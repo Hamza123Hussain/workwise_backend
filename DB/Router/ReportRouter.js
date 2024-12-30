a7e3894052cd798dc05f8bcb0c7b0ae1bda4cef3
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-30 17:02:38 +0500

    Update ReportRouter.js

--- DB/Router/ReportRouter.js
+++ DB/Router/ReportRouter.js
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import { Get_A_Single_Report } from '../Report/GetASingleReport.js'
 import { GetAllReports } from '../Report/GetReports.js'
+import { CreateReport } from '../Controller/Report/CreateReport.js'
 
 export const ReportRouter = Router()
 ReportRouter.get('/UserReport', Get_A_Single_Report)
 ReportRouter.get('/AllReports', GetAllReports)
+ReportRouter.post('/CreateReport', CreateReport)
