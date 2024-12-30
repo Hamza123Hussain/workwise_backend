b6be96528e49889ae4f13ae08f6b6662662faf6a
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-30 17:03:15 +0500

    Update FetchMessage.js

--- DB/Controller/Messages/FetchMessage.js
+++ DB/Controller/Messages/FetchMessage.js
@@ -1,16 +1,12 @@
 import { ref, get } from 'firebase/database'
 import { database } from '../../../FireBaseConfig.js'
-
 export const GetMessages = async (req, res) => {
   const { chatId } = req.query // Get chatId from URL parameters
-
   try {
     // Reference to the messages of the specific chat
     const chatRef = ref(database, `chats/${chatId}/messages`)
-
     // Get the snapshot of the messages
     const snapshot = await get(chatRef)
-
     if (snapshot.exists()) {
       // Convert the messages object to an array
       const messagesObject = snapshot.val()
@@ -20,7 +16,6 @@ export const GetMessages = async (req, res) => {
           ...data, // Spread the rest of the message properties
         })
       )
-
       // Respond with the transformed array
       res.status(200).json({
         chatId,
