e95b216a7065fca1b47593866d9005abbc50a749
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-01-07 12:56:48 +0500

    added KPIS

--- DB/Router/KPIROUTER.js
+++ DB/Router/KPIROUTER.js
@@ -1,9 +1,13 @@
 import { Router } from 'express'
 import { KPIMaker } from '../Controller/KPI/CreateKpi.js'
+import { DeleteAKPI } from '../Controller/KPI/DeleteKpi.js'
+import { GetAllKpi } from '../Controller/KPI/GetAllKpi.js'
+import { GetAKPI } from '../Controller/KPI/GetASingleKpi.js'
+import { UpdateKpi } from '../Controller/KPI/UpdateKPi.js'
 
 export const KPIROUTER = Router()
 KPIROUTER.post('/CreateNewKPI', KPIMaker)
-KPIROUTER.delete('/DeleteKpi')
-KPIROUTER.get('/GetAllKPIs')
-KPIROUTER.get('/SingleKpi')
-KPIROUTER.put('/UpdateKPI')
+KPIROUTER.delete('/DeleteKpi', DeleteAKPI)
+KPIROUTER.get('/GetAllKPIs', GetAllKpi)
+KPIROUTER.get('/SingleKpi', GetAKPI)
+KPIROUTER.put('/UpdateKPI', UpdateKpi)
