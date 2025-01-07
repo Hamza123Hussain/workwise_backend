545b4539b947b37ad6cdeb8eb55b54854e35f529
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-01-07 17:28:02 +0500

    calculation fixed

--- DB/Controller/KPI/TargetPrioirty.js
+++ DB/Controller/KPI/TargetPrioirty.js
@@ -3,11 +3,11 @@ export const PointsGained_BasedOnPriority = (Target) => {
   const UpdatedTarget = Target.map((element) => {
     // Check the priority and assign PointsGained accordingly
     if (element.Priority === 'High') {
-      element.TotalPoints = 10 // Assign 10 points for high-priority targets
+      element.TotalPoints = 10 * element.TargetValue // Assign 10 points for high-priority targets
     } else if (element.Priority === 'Medium') {
-      element.TotalPoints = 5 // Assign 5 points for medium-priority targets
+      element.TotalPoints = 5 * element.TargetValue // Assign 5 points for medium-priority targets
     } else {
-      element.TotalPoints = 2.5 // Assign 2.5 points for low or other priorities
+      element.TotalPoints = 2.5 * element.TargetValue // Assign 2.5 points for low or other priorities
     }
     element.PointsGained = 0
     // Return the updated element for inclusion in the new array
