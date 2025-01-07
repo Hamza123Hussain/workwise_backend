bc2f058e0213e3c646c4769935699d809e7c22c7
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-01-07 14:43:48 +0500

    updating points gained

---
+++ DB/Controller/KPI/UpdatePointsGained.js
@@ -0,0 +1,16 @@
+export const UpdatingPointsGained = (Target, _id) => {
+  // Use map to iterate through the array and update each target
+  const UpdatedTarget = Target.map((element) => {
+    // Check the priority and assign PointsGained accordingly
+    if (element._id === _id) {
+      element.PointsGained =
+        element.TotalPoints * (element.TargetValue / element.ValueAchieved)
+    }
+
+    // Return the updated element for inclusion in the new array
+    return element
+  })
+
+  // Return the updated array with modified PointsGained values
+  return UpdatedTarget
+}
