344d4519c7036edc4f6493dc4854fe84a3d6e30c
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-01-08 18:02:32 +0500

    Create UserTasks.js

---
+++ DB/Models/UserTasks.js
@@ -0,0 +1,58 @@
+import mongoose from 'mongoose'
+
+// Define the Task Schema
+const TaskSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  assignedTo: {
+    type: String,
+    required: true,
+  },
+  priority: {
+    type: String,
+    enum: ['LOW', 'MEDIUM', 'HIGH'],
+    default: 'MEDIUM',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  dueDate: {
+    type: Date,
+  },
+  description: {
+    type: String,
+  },
+  TaskCompletion: {
+    type: Number,
+    default: 0,
+  },
+  TotalPoints: {
+    type: Number,
+    default: 0, // Default value for TotalPoints
+  },
+  PointsGained: {
+    type: Number,
+    default: 0, // Default value for PointsGained
+  },
+  TaskType: {
+    type: String,
+    enum: ['Daily', 'Weekly', 'Other'],
+    default: 'Daily',
+  },
+  UpdatedBY: {
+    type: String,
+  },
+  UpdatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+// Create and export the Task model
+export const TaskModel = mongoose.model('Task', TaskSchema)
