3165cee78689c379081b2715df433fe5662ac93e
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-01-16 17:38:53 +0500

    updated model

--- DB/Models/employeetasks.js
+++ DB/Models/employeetasks.js
@@ -5,35 +5,39 @@ const UserTaskSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  Tasks: [
-    {
-      TaskName: {
-        type: String,
-        required: true,
-      },
-      Priority: {
-        type: String,
-        enum: ['High', 'Medium', 'Low'],
-        required: true,
-      },
-      PointsGained: {
-        type: Number,
-        default: 0,
-      },
-      TotalPoints: {
-        type: Number,
-        required: true,
-      },
-      Description: {
-        type: String,
-        required: true,
-      },
-      DueDate: {
-        type: Date,
-        required: true,
-      },
-    },
-  ],
+  TaskName: {
+    type: String,
+    required: true,
+  },
+  Priority: {
+    type: String,
+    enum: ['High', 'Medium', 'Low'],
+    required: true,
+  },
+  PointsGained: {
+    type: Number,
+    default: 0,
+  },
+  TotalPoints: {
+    type: Number,
+    required: true,
+  },
+  Description: {
+    type: String,
+    required: true,
+  },
+  DueDate: {
+    type: Date,
+    required: true,
+  },
+  UserName: {
+    type: String, // Added field to store the user's name
+    required: true,
+  },
+  UserEmail: {
+    type: String, // Added field to store the user's email
+    required: true,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
