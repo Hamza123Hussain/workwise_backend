19d61230c20769192b3b8b8739308b8208df370d
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-02-04 14:18:02 +0500

    Update UpdateCompletion.js

--- DB/Controller/usertasks/UpdateCompletion.js
+++ DB/Controller/usertasks/UpdateCompletion.js
@@ -1,7 +1,7 @@
 import UserTask from '../../Models/employeetasks.js'
-export const updateTask = async (req, res) => {
-  const { UserId, TaskId } = req.params // Extract UserId and TaskId from request parameters
-  const { Completed } = req.body
+export const updateCompletion = async (req, res) => {
+  const { UserId, TaskId } = req.query // Extract UserId and TaskId from request parameters
+  const { Completed, TotalPoints } = req.body
   try {
     // Find the task document and update it
     const updatedTask = await UserTask.findOneAndUpdate(
@@ -9,6 +9,7 @@ export const updateTask = async (req, res) => {
       {
         $set: {
           Completed,
+          PointsGained: TotalPoints,
         },
       },
       { new: true } // Return the updated task document
