fc1bb4362a3ac6d2dd00d5c4601290a8b5d2b5b8
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-02-04 14:17:56 +0500

    Update RoleTaskRouter.js

--- DB/Router/RoleTaskRouter.js
+++ DB/Router/RoleTaskRouter.js
@@ -4,9 +4,13 @@ import { UpdateRoleTasks } from '../Controller/RoleTask/UpdateTasksAndRole.js'
 import { GetARole } from '../Controller/RoleTask/GetSingleRole.js'
 import { GetAllRoles } from '../Controller/RoleTask/GetAllRoles.js'
 import { DeleteARoleTask } from '../Controller/RoleTask/DeleteRoleTask.js'
+import { updateDescription } from '../Controller/usertasks/UpdateDescription.js'
+import { updateCompletion } from '../Controller/usertasks/UpdateCompletion.js'
 export const RoleTaskRouter = Router()
 RoleTaskRouter.post('/CreateNewTask', CreateRoleTasks)
 RoleTaskRouter.put('/UpdateTask', UpdateRoleTasks)
 RoleTaskRouter.get('/GetAllRoleTasks', GetAllRoles)
 RoleTaskRouter.get('/GetSingleRoleTask', GetARole)
 RoleTaskRouter.delete('/DeleteTask', DeleteARoleTask)
+RoleTaskRouter.put('/UpdateDescription', updateDescription)
+RoleTaskRouter.put('/UpdateCompletion', updateCompletion)
