84be3944d175dd5d0352d966e8e44288f3b3d26f
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-02-18 17:52:02 +0500

    Update FetchMessage.js

--- DB/Controller/Messages/FetchMessage.js
+++ DB/Controller/Messages/FetchMessage.js
@@ -1,10 +1,20 @@
 import { ref, get } from 'firebase/database'
 import { database } from '../../../FireBaseConfig.js'
+import ChatModel from '../../Models/ChatModel.js'
 export const GetMessages = async (req, res) => {
-  const { chatId } = req.query // Get chatId from URL parameters
+  const { UserID, RecipentID } = req.query // Get chatId from URL parameters
+  const ExistingChat = await ChatModel.findOne({
+    $or: [
+      { UserID, RecipentID },
+      { UserID: RecipentID, RecipentID: UserID },
+    ],
+  })
+  if (!ExistingChat) {
+    return res.status(404).json('NO CHATS FOUND FOR THIS USER')
+  }
   try {
     // Reference to the messages of the specific chat
-    const chatRef = ref(database, `chats/${chatId}/messages`)
+    const chatRef = ref(database, `chats/${ExistingChat.ChatID}/messages`)
     // Get the snapshot of the messages
     const snapshot = await get(chatRef)
     if (snapshot.exists()) {
@@ -18,13 +28,12 @@ export const GetMessages = async (req, res) => {
       )
       // Respond with the transformed array
       res.status(200).json({
-        chatId,
         messages: messagesArray,
       })
     } else {
       // If no messages exist, return a message indicating so
       res.status(404).json({
-        message: `No messages found for chat ID: ${chatId}.`,
+        message: `No messages found for chat ID: ${ExistingChat.ChatID}.`,
       })
     }
   } catch (error) {
