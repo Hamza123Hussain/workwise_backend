c8370eb5726f2a25fd9aaca9a2b1007ec9a5cca1
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-03-05 14:05:19 +0500

    Update GetUserTasks.js

--- DB/Controller/usertasks/GetUserTasks.js
+++ DB/Controller/usertasks/GetUserTasks.js
@@ -4,8 +4,8 @@ export const getUserTasks = async (req, res) => {
   const { UserId } = req.query
 
   try {
-    // Find all tasks for the specified UserId
-    const userTasks = await UserTask.find({ UserId })
+    // Find all tasks for the specified UserId and sort by createdAt in descending order
+    const userTasks = await UserTask.find({ UserId }).sort({ createdAt: -1 })
 
     if (!userTasks || userTasks.length === 0) {
       return res.status(200).json([])
@@ -13,7 +13,7 @@ export const getUserTasks = async (req, res) => {
 
     res.status(200).json({
       message: 'Tasks retrieved successfully',
-      data: userTasks, // Return the array of tasks
+      data: userTasks, // Return the array of tasks sorted by latest first
     })
   } catch (error) {
     console.error('Error fetching tasks:', error)
