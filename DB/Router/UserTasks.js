4840bbfdfc8ec03465ae15264d04d330d8caecd0
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-05-01 15:10:21 +0500

    Update UserTasks.js

--- DB/Router/UserTasks.js
+++ DB/Router/UserTasks.js
@@ -5,22 +5,19 @@ import { updateTask } from '../Controller/usertasks/UpdateTask.js'
 import { deleteTask } from '../Controller/usertasks/DeleteTask.js'
 import { getUserTasks } from '../Controller/usertasks/GetUserTasks.js'
 import { getAllTasks } from '../Controller/usertasks/AllTasks.js'
-
+import { getUserTasksForCurrentMonth } from '../Controller/usertasks/GetTasksForCurrentMonth.js'
+import { getUserTasksForEachMonth } from '../Controller/usertasks/GetTasksForEachMonth.js'
 export const UserTaskRouter = Router()
-
 // Create a new task for a user
 UserTaskRouter.post('/CreateNewTask', createTask)
-
 // Get all tasks for a user
 UserTaskRouter.get('/GetUserTasks', getUserTasks)
-
 // Get a single task by taskId and userId
 UserTaskRouter.get('/:UserId/SingleTask/:TaskId', getTask)
-
 // Update a specific task by taskId and userId
 UserTaskRouter.put('/:UserId/UpdateTask/:TaskId', updateTask)
-
 // Delete a specific task by taskId and userId
 UserTaskRouter.delete('/DeleteTask', deleteTask)
-
 UserTaskRouter.get('/AllTasks', getAllTasks)
+UserTaskRouter.get('/GetTasksForCurrentMonth', getUserTasksForCurrentMonth)
+UserTaskRouter.get('/GetTasksForEachMonth', getUserTasksForEachMonth)
