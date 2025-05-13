afcfa3a954f0cedb4fb83366c2e04b72c37eb39e
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-05-13 18:48:11 +0500

    Update GetAllKpi.js

--- DB/Controller/KPI/GetAllKpi.js
+++ DB/Controller/KPI/GetAllKpi.js
@@ -4,10 +4,6 @@ import { User } from '../../Models/User.js'
 export const GetAllKpi = async (req, res) => {
   const { UserID } = req.query
   try {
-    const ExisitngUser = await User.findById(UserID)
-    if (!ExisitngUser) {
-      res.status(500).json('You are not authorized to use this api')
-    }
     const AllKpis = await KPIModel.find()
     res.status(200).json(AllKpis)
   } catch (error) {
