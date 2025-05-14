919b43c9ae417b8e06d48aa80455b154fd76b3e9
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-05-14 16:14:41 +0500

    Create GetAllUserNames.js

---
+++ DB/Controller/KPI/GetAllUserNames.js
@@ -0,0 +1,19 @@
+import { KPIModel } from '../../Models/kpi.js'
+export const GetAllUserNames = async (req, res) => {
+  const { UserEmail } = req.query
+  try {
+    if (
+      !UserEmail === 'gptprompts87@gmail.com' ||
+      !UserEmail === 'globalgrads.org@gmail.com' ||
+      !UserEmail === 'octtoppus1@gmail.com'
+    ) {
+      res.status(500).json('You are not authorized to use this api')
+    }
+    const AllKpis = await KPIModel.find().select('UserName')
+    res.status(200).json(AllKpis)
+  } catch (error) {
+    res
+      .status(500)
+      .json({ Message: 'Some Error Ocurred In Fetching Kpis', error })
+  }
+}
