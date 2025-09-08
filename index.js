4e290e740c8b669444d7fcfc55e4cc5e404b6e81
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-09-08 14:24:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ import NoticeRouter from './DB/Router/NoticeRouter.js'
 import { KPIROUTER } from './DB/Router/KPIROUTER.js'
 import { RoleTaskRouter } from './DB/Router/RoleTaskRouter.js'
 import { UserTaskRouter } from './DB/Router/UserTasks.js'
+import { InvoiceRouter } from './DB/Router/InvoiceRouter.js'
 const app = express()
 app.use(express.json())
 const corsOptions = {
@@ -33,6 +34,7 @@ app.use('/Api/Notice', NoticeRouter)
 app.use('/Api/KPI', KPIROUTER)
 app.use('/Api/RoleTask', RoleTaskRouter)
 app.use('/Api/UserTask', UserTaskRouter)
+app.use('/Api/Invoice', InvoiceRouter)
 DB_CONNECTED()
 app.listen(PORT, () => {
   console.log('port is on')
