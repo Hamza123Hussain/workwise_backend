14bf23b7f2498c6fbb06c5c070be9847f68865ff
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-09-09 13:35:17 +0500

    Update Invoice.js

--- DB/Models/Invoice.js
+++ DB/Models/Invoice.js
@@ -4,10 +4,10 @@ const InvoiceSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   date: { type: Date, default: Date.now },
-  ApprovalStatus: { type: Boolean, default: false },
-  CompanyName: { type: String, default: '' },
-  CreatedBy: { type: String }, // Optional field for who created the Invoice
-  FinancialYear: { type: String },
+  approvalStatus: { type: Boolean, default: false },
+  companyName: { type: String, default: '' },
+  createdBy: { type: String }, // Optional field for who created the Invoice
+  financialYear: { type: String },
 })
 
 export const InvoiceModel = mongoose.model('Invoice', InvoiceSchema)
