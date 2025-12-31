b677c6c8961a2cb45aa7202c9fadbe98e6b49bee
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-12-31 12:12:30 +0500

    Update GetAllRoles.js

--- DB/Controller/RoleTask/GetAllRoles.js
+++ DB/Controller/RoleTask/GetAllRoles.js
@@ -3,16 +3,20 @@ import { User } from '../../Models/User.js'
 
 export const GetAllRoles = async (req, res) => {
   const { UserID } = req.query
+
   try {
-    const ExisitngUser = await User.findById(UserID)
-    if (!ExisitngUser && ExisitngUser._id !== 'qdyyLorl9WR27GcAxkBbxHO4sWu1') {
-      res.status(500).json('You are not authorized to use this api')
+    const existingUser = await User.findById(UserID)
+
+    if (
+      !existingUser ||
+      existingUser._id.toString() !== 'qdyyLorl9WR27GcAxkBbxHO4sWu1'
+    ) {
+      return res.status(401).json('You are not authorized to use this API')
     }
-    const AllRoles = await RoleTasks.find()
-    res.status(200).json(AllRoles)
+
+    const roles = await RoleTasks.find()
+    res.status(200).json(roles)
   } catch (error) {
-    res
-      .status(500)
-      .json({ Message: 'Some Error Ocurred In Fetching Kpis', error })
+    res.status(500).json({ Message: 'Error fetching roles', error })
   }
 }
