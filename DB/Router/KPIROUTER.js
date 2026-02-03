708584c90175c28e003f75b7b3c4c6ac62d5abc2
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2026-02-03 13:12:46 +0500

    .

--- DB/Router/KPIROUTER.js
+++ DB/Router/KPIROUTER.js
@@ -6,6 +6,7 @@ import { GetAKPI } from '../Controller/KPI/GetASingleKpi.js'
 import { GetUserDetailsOnly } from '../Controller/KPI/GettingUserDetailsOnly.js'
 import { GetAllUserNames } from '../Controller/KPI/GetAllUserNames.js'
 import { UpdateKPI } from '../Controller/KPI/UpdateKPi.js'
+import { UpdateKpiTaskboard } from '../Controller/KPI/UpdateKpiTaskboard.js'
 
 export const KPIROUTER = Router()
 KPIROUTER.post('/CreateNewKPI', KPIMaker)
@@ -13,5 +14,6 @@ KPIROUTER.delete('/DeleteKpi', DeleteAKPI)
 KPIROUTER.get('/GetAllKPIs', GetAllKpi)
 KPIROUTER.get('/SingleKpi', GetAKPI)
 KPIROUTER.put('/UpdateKPI', UpdateKPI)
+KPIROUTER.put('/UpdateKpiForTaskBoard', UpdateKpiTaskboard)
 KPIROUTER.get('/useronly', GetUserDetailsOnly)
 KPIROUTER.get('/GetAllUserNames', GetAllUserNames)
